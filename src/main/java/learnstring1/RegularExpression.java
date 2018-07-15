3d77dc123ac9a73e2839529139b488a220f3d00c
HEMALATHAA R N <HEMALATHAA R N@DELL-PC>
2018-07-15 10:04:17 +0530

    sample jave program1

--- src/main/java/learnstring1/RegularExpression.java
+++ src/main/java/learnstring1/RegularExpression.java
@@ -4,13 +4,17 @@ public class RegularExpression {
 
 	public static void main(String[] args) {
 /*		String text = "I am working in SCB Global services(1520112) with 20 projects";
+ //Program 1
 System.out.println(text.replaceAll("[\\D]", ""));
+//program 2
 		System.out.println(text.replaceAll("[a-zA-Z0-9]", "").replaceAll("[\\s]", ""));
 	*/	
+		//Program 3:
+		
 		String text = "45800054$";
 		String value1= text.replaceAll("\\D", "");
 		System.out.println(value1.substring(0, 5));
-		
 	}
 
 }
+
